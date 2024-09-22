5fe146047bf74f253e7f8cbb22190e2ca6c8e50a
Mario Villacreses <MarioVillacreses@outlook.com>
2024-09-22 00:56:46 +0000

    Remove deprecated components and pages

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,3 @@
-export * from './InlineLink';
-export * from './ItemListExperience';
-export * from './ItemListSocialLinks';
-export * from './ItemListStats';
-export * from './Icon';
-export * from './BubbleList';
-export * from './ItemListProjects';
-
-export type { ExperienceEntryProps } from './ItemListExperience';
+export * from "./InlineLink";
+export * from "./Icon";
+export * from "./BubbleList";
