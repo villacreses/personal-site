20fe6feed5de44cef04608093af30f4116d4ceb2
Mario Villacreses <MarioVillacreses@outlook.com>
2024-10-03 19:09:29 +0000

    Footer styles & secret text

--- src/components/PageFooter.tsx
+++ src/components/PageFooter.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { ComponentProps } from "react";
 import Markdown from "react-markdown";
 
@@ -8,18 +9,34 @@ Powered by [Next.js](https://nextjs.org/) & inspired by [Blowfish](https://blowf
 `;
 
 const markdownComponents: ComponentProps<typeof Markdown>["components"] = {
-  p: ({ node, ...props }) => <p className="last:text-right" {...props} />,
+  p: ({ node, ...props }) => (
+    <p className="last:text-right opacity-70" {...props} />
+  ),
 };
 
 export default function PageFooter() {
   return (
-    <footer className="py-10 print:hidden">
+    <footer className="mt-auto pt-20 pb-10 print:hidden relative">
       <Markdown
         className="text-xs text-neutral-500 dark:text-neutral-400 flex flex-row justify-between"
         components={markdownComponents}
       >
         {content}
       </Markdown>
+      <small
+        aria-hidden="true"
+        className="block absolute bottom-0 inset-x-0 text-transparent cursor-default"
+      >
+        Still using Netscape? Check out my{" "}
+        <Link
+          href="/90s"
+          tabIndex={-1}
+          className="text-transparent underline dark:text-transparent hover:text-transparent dark:hover:text-transparent cursor-not-allowed"
+        >
+          optimized site
+        </Link>
+        .
+      </small>
     </footer>
   );
 }
