7a1f66454cfa69a1dce5e31a4d75398e5e76a056
Mario Villacreses <MarioVillacreses@outlook.com>
2024-10-03 19:17:52 +0000

    Markdown components: h1, a

--- src/components/Markdown.tsx
+++ src/components/Markdown.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { ComponentProps, ReactNode } from "react";
 import MarkdownBase from "react-markdown";
 
@@ -10,6 +11,16 @@ const defaultComponents: MarkdownComponents = {
   },
   li({ node, ...props }) {
     return <li className="mb-3" {...props} />;
+  },
+  a({ node, href = "#", ...props }) {
+    return <Link href={href} {...props} />;
+  },
+  h1({
+    node,
+    className = "text-4xl font-extrabold text-center mb-5",
+    ...props
+  }) {
+    return <h1 className={className} {...props} />;
   },
 };
 
