283de72e26343806e7ff4dcdaa22905ae64e7753
Mario Villacreses <MarioVillacreses@outlook.com>
2024-10-19 15:57:48 +0000

    Create PageService

---
+++ src/middleware.ts
@@ -0,0 +1,9 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const headers = new Headers(request.headers);
+
+  headers.set("x-current-path", request.nextUrl.pathname);
+
+  return NextResponse.next({ headers });
+}
