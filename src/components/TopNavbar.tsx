2ee30e887038e000e0f3476ab4a7e706d282d5a6
Mario Villacreses <MarioVillacreses@outlook.com>
2024-10-23 17:25:10 +0000

    Bug fixes

--- src/components/TopNavbar.tsx
+++ src/components/TopNavbar.tsx
@@ -2,7 +2,6 @@
 
 import DefaultLink from "next/link";
 import { ComponentProps, FC } from "react";
-import { useScrollTracker } from "@/hooks";
 import BurgerMenu from "./BurgerMenu";
 import { ToggleDarkMode } from "./ToggleDarkMode";
 
@@ -38,16 +37,12 @@ const Link: FC<ComponentProps<typeof DefaultLink>> = (props) => (
 );
 
 export default function TopNavbar() {
-  const { scrolledToTop } = useScrollTracker();
-
-  const navbarBlurOpacity = scrolledToTop ? 0 : 1;
-
   return (
     <>
       <div className="fixed inset-x-0 px-6 sm:px-12 py-2 mb-[95px] h-[53px]">
         <div
           className="absolute inset-x-0 top-0 h-full backdrop-blur-2xl"
-          style={{ opacity: navbarBlurOpacity }}
+          style={{ opacity: 1 }}
         />
         <div className="m-auto max-w-[64rem] flex flex-row items-center relative">
           <nav className="mr-auto">
